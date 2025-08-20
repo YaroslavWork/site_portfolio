a8272d042ec14cee1d35299287714a4e201ce02f
YaroslavWork <zahorodnyi.yaroslav.work@gmail.com>
2025-08-20 09:48:17 +0200

    Add useSystemTheme hook for dynamic theme management; update App component to utilize the hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import React, { useEffect } from 'react';
+
 import './App.css';
-import Button from './components/Button/Button';
+
+import { useEducationData } from './features/hooks/index.hooks';
+import { useSystemTheme } from './hooks/useSystemTheme';
 import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
+  const systemTheme = useSystemTheme();
+
+  const activeTheme = systemTheme;
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', activeTheme);
+  }, [activeTheme]);
+
   return (
     <>
     <HomePage />
