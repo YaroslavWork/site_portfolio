93ea2a0bf53a8685de3366bfa0be50a000175698
YaroslavWork <zahorodnyi.yaroslav.work@gmail.com>
2025-08-20 09:51:46 +0200

    Refactor theme hook import path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import './App.css';
 
 import { useEducationData } from './features/hooks/index.hooks';
-import { useSystemTheme } from './hooks/useSystemTheme';
+import { useSystemTheme } from './features/hooks/useSystemTheme.hooks';
 import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
