ca6d562d9c8be0d4ffcda56daca1b9d16e5cbb44
YaroslavWork <zahorodnyi.yaroslav.work@gmail.com>
2025-09-23 13:01:30 +0200

    Change visibilty of skill sections if text is not provided (closes #28)

--- frontend/src/components/SkillField/SkillField.js
+++ frontend/src/components/SkillField/SkillField.js
@@ -14,20 +14,26 @@ export default function SkillField({
 }) {
 
   const isUsedInProjectVisible = usedInProjects.length === 0 ? false : true;
+  const isWhatILearnVisible = whatImLearningText.length === 0 ? false : true;
+  const isWhatIPlanVisible = whatIPlanText.length === 0 ? false : true;
 
   return (
     <BorderContainer>
         <div className={skills.technologyTop}>
-            <h1 className={skills.technologyName}>{technologyName}</h1>
-            <p className={skills.technologyType}>{technologyType}</p>
+          <h1 className={skills.technologyName}>{technologyName}</h1>
+          <p className={skills.technologyType}>{technologyType}</p>
         </div>
         <div className={skills.whatField}>
-            {isUsedInProjectVisible &&
-              <SkillSubComponentProjects title={usedInProjectsTitle} projectsName={usedInProjects} color={color}/>
-            }
-            <SkillSubComponent whatType={whatIKnowTitle} text={whatIKnowText} color={color}/>
+          {isUsedInProjectVisible &&
+            <SkillSubComponentProjects title={usedInProjectsTitle} projectsName={usedInProjects} color={color}/>
+          }
+          <SkillSubComponent whatType={whatIKnowTitle} text={whatIKnowText} color={color}/>
+          {isWhatILearnVisible &&
             <SkillSubComponent whatType={whatImLearningTitle} text={whatImLearningText} color={color}/>
-            <SkillSubComponent whatType={whatIPlanTitle} text={whatIPlanText} color={color}/>
+          }
+          {isWhatIPlanVisible &&
+          <SkillSubComponent whatType={whatIPlanTitle} text={whatIPlanText} color={color}/>
+          }
         </div>
     </BorderContainer>
   )
