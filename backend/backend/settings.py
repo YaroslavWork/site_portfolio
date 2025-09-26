3fb7d4f4d393de2b6de52e29afa472e5a06f8e44
YaroslavWork <zahorodnyi.yaroslav.work@gmail.com>
2025-09-26 15:41:40 +0200

    Add dockerfiles, change settings on backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(str(BASE_DIR / '.env'))
 
+# This is the absolute path to the directory where collectstatic will gather static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
